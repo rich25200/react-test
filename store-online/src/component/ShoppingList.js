f954c3d51afe3a298e7f7d6a97045b020993f00b
kazou <thebestofrichou@hotmail.com>
2021-09-06 16:06:46 +0200

    plant list

--- store-online/src/component/ShoppingList.js
+++ store-online/src/component/ShoppingList.js
@@ -1,16 +1,18 @@
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+import { plantList } from '../datas/plantList'
 
 function ShoppingList() {
+
+    const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	);
+    
+    console.log(categorie);
     return (
         <ul>
-            {plantList.map((plant) => (
-                <li>{plant}</li>
+            {plantList.map((plant, index) => (
+                <li key={plant.id}>{plant}</li> 
             ))}
         </ul>
     )
