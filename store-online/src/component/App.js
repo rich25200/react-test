c70f8a3e6ff99b9f5c0038f18394473bdaf7dc33
rich25200 <rich25200@users.noreply.github.com>
2021-09-06 22:56:15 +0200

    props

--- store-online/src/component/App.js
+++ store-online/src/component/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div>
       <Banner />
-      <Cart />
       <ShoppingList />
+      <Cart />
     </div>
   );
 }
--- store-online/src/component/ShoppingList.js
+++ store-online/src/component/ShoppingList.js
@@ -1,21 +1,30 @@
 import { plantList } from '../datas/plantList'
+import '../styles/ShoppingList.css'
 
 function ShoppingList() {
-
-    const categories = plantList.reduce(
+	const categories = plantList.reduce(
 		(acc, plant) =>
 			acc.includes(plant.category) ? acc : acc.concat(plant.category),
 		[]
-	);
-    
-    console.log(categorie);
-    return (
-        <ul>
-            {plantList.map((plant, index) => (
-                <li key={plant.id}>{plant}</li> 
-            ))}
-        </ul>
-    )
+	)
+
+	return (
+		<div>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map((plant) => (
+					<li key={plant.id} className='lmj-plant-item'>
+						{plant.name}
+						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
